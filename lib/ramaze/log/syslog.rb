eef1652a55e859934c57465c3ff4bc71f64d12e8
Arnaud Meuret <arnaud@meuret.name>
2012-07-13 18:35:20 +0200

    Ramaze::Logger::Syslog now escapes printf % sequences in messages

--- lib/ramaze/log/syslog.rb
+++ lib/ramaze/log/syslog.rb
@@ -1,16 +1,8 @@
 #          Copyright (c) 2008 Michael Fellinger m.fellinger@gmail.com
 #          Copyright (c) 2008 rob@rebeltechnologies.nl
-# All files in this distribution are subject to the terms of the Ruby license.
+# All files in this distribution are subject to the terms of the MIT license.
 
 require 'syslog'
-
-# Add aliases for the levelnames used by Ramaze logging
-module Syslog
-    alias dev debug
-    alias warn warning
-    alias error err
-    module_function :dev, :warn, :error 
-end
 
 module Ramaze
   module Logger
@@ -21,19 +13,25 @@ module Ramaze
     class Syslog
       include Logging
 
+      # Hash containing various method aliases. Rbx and Jruby don't seem to like
+      # the combination of alias() and module_function() so this works around
+      # that.
+      ALIASES = {:dev => :debug, :warn => :warning, :error => :err}
+
       ##
       # Open the syslog library, if it is allready open, we reopen it using the
-      # new argument list.  The argument list is passed on to the Syslog library
-      # so please check that, and man syslog for detailed information.  
+      # new argument list. The argument list is passed on to the Syslog library
+      # so please check that, and man syslog for detailed information.
+      #
       # There are 3 parameters:
       #
-      #   ident:  The identification used in the log file, defaults to $0
-      #   options:  defaults to  Syslog::LOG_PID | Syslog::LOG_CONS
-      #   facility: defaults to Syslog::LOG_USER
+      # * ident:  The identification used in the log file, defaults to $0
+      # * options:  defaults to  Syslog::LOG_PID | Syslog::LOG_CONS
+      # * facility: defaults to Syslog::LOG_USER
       #
-      def initialize( *args )
+      def initialize(*args)
         ::Syslog.close if ::Syslog.opened?
-        ::Syslog.open( *args )
+        ::Syslog.open(*args)
       end
 
       ##
@@ -43,6 +41,12 @@ module Ramaze
       # be done by the syslog daemon based on it's configuration.
       def log(tag, *messages)
         return if !::Syslog.opened?
+        tag = tag.to_sym
+
+        if ALIASES.key?(tag)
+          tag = ALIASES[tag]
+        end
+
         messages = messages.map {|m| m.gsub(/(%[^m])/,'%\1')}
         ::Syslog.send(tag, *messages)
       end
@@ -53,6 +57,6 @@ module Ramaze
       def inspect
         ::Syslog.inspect
       end
-    end
-  end
-end
+    end # Syslog
+  end # Logger
+end # Ramaze
